17c3e1f9ef539c860d4eee2cdf0ff0c8f8367bab
Volodymyr0077 <v.pisarzhevsky@gmail.com>
2020-11-01 10:17:48 -0600

    Me flying

--- src/main/java/Superman.java
+++ src/main/java/Superman.java
@@ -2,4 +2,9 @@ public class Superman {
     public void fly(){
         System.out.println("Superman is flying");
     }
+
+    public static void main(String[] args) {
+        Superman me=new Superman();
+        me.fly();
+    }
 }
