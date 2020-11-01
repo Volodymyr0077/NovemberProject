d9435a42f8f56f8a89afcf7d0eade4fccf54a84a
Volodymyr0077 <v.pisarzhevsky@gmail.com>
2020-11-01 10:23:20 -0600

    Laser sight

--- src/main/java/Superman.java
+++ src/main/java/Superman.java
@@ -2,9 +2,14 @@ public class Superman {
     public void fly(){
         System.out.println("Superman is flying");
     }
+    public void laserSight(){
+        System.out.println("using laser sight");
+    }
 
     public static void main(String[] args) {
         Superman me=new Superman();
         me.fly();
+        me.laserSight();
+
     }
 }
